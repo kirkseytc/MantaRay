ded2c2101ee64698d88a264a586f8810a320e6d3
kirkseytc <112491254+kirkseytc@users.noreply.github.com>
2023-01-09 22:42:58 -0500

    ver 0.1.1 update

--- mantaRayProjectFiles/application.java
+++ mantaRayProjectFiles/application.java
@@ -7,22 +7,32 @@ public class application {
 
         boolean contin = true;
 
-        System.out.println("Welcome to the Ray Compression Tool [CLI Edition]\nVer 0.1 : The \"Barely Functional\" Update | Tristan Kirksey\n");
+        String filePath1;
+        String filePath2;
+        String name;
+
+        System.out.println("Welcome to the Ray Compression Tool [CLI Edition]\nVer 0.1.1 : The \"Barely Functional\" Update | Tristan Kirksey\n");
 
         while(contin){
 
             System.out.print("Would you like to compress a file/folder or decompress a ray archive? ([c]/d): ");
             String choice = scnr.nextLine();
 
-            if(choice.indexOf("d") >= 0 || choice.indexOf("D") >= 0){
-                System.out.print("What is the path of the '.ray' file to be uncompressed?: ");
-                choice = scnr.nextLine();
-                mantaRay.decompress(choice); 
+            if(choice.indexOf("d") >= 0 || choice.indexOf("D") >= 0){ // DECOMPRESS
+                System.out.print("What is the path of the '.ray' file to be decompressed?: ");
+                filePath1 = scnr.nextLine();
+                System.out.print("Where do you want the archive to decompress at? : ");
+                filePath2 = scnr.nextLine();
+                mantaRay.decompress(filePath1, filePath2); 
             }
-            else{
-                System.out.print("What is the file/folder path of the file to be compressed?: ");
-                choice = scnr.nextLine();
-                mantaRay.compress(choice);
+            else{ // COMPRESS
+                System.out.print("What is the file/folder path to be compressed?: ");
+                filePath1 = scnr.nextLine();
+                System.out.print("Where do you want the '.ray' file to export at?: ");
+                filePath2 = scnr.nextLine();
+                System.out.print("What would you like to name the archive? (if left blank, it will use the folder/file name): ");
+                name = scnr.nextLine();
+                mantaRay.compress(filePath1, filePath2, name);
             }
 
             System.out.print("Do you want to perform another operation?: (y/[n]): ");
--- mantaRayProjectFiles/mantaRay.java
+++ mantaRayProjectFiles/mantaRay.java
@@ -2,9 +2,10 @@ public class mantaRay {
     
     private static boolean isFile;
 
-    public static void compress(String filePath){
-        System.out.println(filePath);
-        if(filePath.indexOf(".") > 0){
+    // filePath1 --> the path to the file/folder to be archived.
+    // filePath2 --> the path to where the .ray archive will be exported. 
+    public static void compress(String filePath1, String filePath2, String name){
+        if(filePath1.indexOf(".") > 0){
             isFile = true;
         }
         else{
@@ -13,9 +14,11 @@ public class mantaRay {
         System.out.println("This path is for a file: " + isFile);
     }
 
-    public static void decompress(String filePath){
-        System.out.println(filePath);
-        if(filePath.indexOf(".ray") > 0){
+    // filePath1 --> the path to the .ray archive.
+    // filePath2 --> the path to the folder in which the files will be exported.
+    public static void decompress(String filePath1, String filePath2){
+        
+        if(filePath1.indexOf(".ray") > 0){
             isFile = true;
         }
         else{
